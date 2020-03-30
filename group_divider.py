96da8348e1322e555f000a4f6624e9f9646f7094
Harry Cutts <eternal.linux@gmail.com>
2020-03-29 20:59:28 -0700

    Allow group sizes to be specified

--- group_divider.py
+++ group_divider.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import random
 
-def divide(names, num_groups):
-    shuffled_names = [*names]
-    random.shuffle(shuffled_names)
+def divide(shuffled_names, num_groups):
     group_size = len(names) // num_groups
     groups = []
     for i in range(0, len(names), group_size):
@@ -14,19 +12,41 @@ def divide(names, num_groups):
 
     return groups
 
+def divide_by_sizes(shuffled_names, group_sizes):
+    groups = []
+    i = 0
+    for size in group_sizes:
+        groups.append(shuffled_names[i:i+size])
+        i += size
+
+    if i != len(shuffled_names):
+        remaining = shuffled_names[i:]
+        print("Warning:", len(remaining), "were left out:", ", ".join(remaining))
+
+    return groups
+
 if __name__ == '__main__':
     import argparse
     import sys
 
     parser = argparse.ArgumentParser(description="Divides a list into random groups")
-    parser.add_argument('--num-groups', type=int, help="The number of groups to create")
     parser.add_argument('names_file', type=argparse.FileType('r'), nargs='?', default=sys.stdin, help="A file containing the names to assign")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--num-groups', type=int, help="The number of groups to create")
+    group.add_argument('--group-sizes', type=str, help="A comma-separated list of the sizes of groups to create")
     args = parser.parse_args()
 
     with args.names_file:
         names = list(map(str.strip, args.names_file.readlines()))
 
-    groups = divide(names, args.num_groups)
+    shuffled_names = [*names]
+    random.shuffle(shuffled_names)
+
+    if args.num_groups is not None:
+        groups = divide(shuffled_names, args.num_groups)
+    else:
+        group_sizes = map(int, args.group_sizes.split(','))
+        groups = divide_by_sizes(shuffled_names, group_sizes)
 
     for i, group in enumerate(groups):
         print("Group {}: {}".format(i, ", ".join(group)))
