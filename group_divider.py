723d49d10b5832d08a764c0f42debf14feb2204a
Harry Cutts <eternal.linux@gmail.com>
2020-03-29 21:30:12 -0700

    Allow group 'members' composed of multiple people
    Since everyone's now joining by video chat, people who live together are
    now using the same computer and therefore can't be put in separate
    groups. Allow these sets of people to be specified together in the input
    files, then use a first-fit bin packing algorithm.
    
    This helper script is rapidly getting out of hand.

--- group_divider.py
+++ group_divider.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
+from collections import namedtuple
 import random
 
-def divide(shuffled_names, num_groups):
-    group_size = len(names) // num_groups
-    groups = []
-    for i in range(0, len(names), group_size):
-        groups.append(shuffled_names[i:i+group_size])
-    if len(groups[-1]) < group_size:
-        groups[-2].extend(groups[-1])
-        del groups[-1]
+GroupMember = namedtuple('GroupMember', ['name', 'size'])
 
-    return groups
+def parse_member(line):
+    return GroupMember(line.strip(), len(line.split('&')))
 
-def divide_by_sizes(shuffled_names, group_sizes):
-    groups = []
-    i = 0
-    for size in group_sizes:
-        groups.append(shuffled_names[i:i+size])
-        i += size
+def divide_by_sizes(members, group_sizes):
+    free_space = list(group_sizes)
+    groups = [[] for i in range(len(free_space))]
+    remaining = []
 
-    if i != len(shuffled_names):
-        remaining = shuffled_names[i:]
-        print("Warning:", len(remaining), "were left out:", ", ".join(remaining))
+    for member in members:
+        assigned = False
+        for i, group in enumerate(groups):
+            if free_space[i] >= member.size:
+                assigned = True
+                group.append(member)
+                free_space[i] -= member.size;
+                break
+
+        if not assigned:
+            remaining.append(member)
+
+    if remaining:
+        print("Warning:", len(remaining), "were left out:", ", ".join(m.name for m in remaining))
 
     return groups
 
@@ -31,22 +35,17 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Divides a list into random groups")
     parser.add_argument('names_file', type=argparse.FileType('r'), nargs='?', default=sys.stdin, help="A file containing the names to assign")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--num-groups', type=int, help="The number of groups to create")
-    group.add_argument('--group-sizes', type=str, help="A comma-separated list of the sizes of groups to create")
+    parser.add_argument('--group-sizes', type=str, help="A comma-separated list of the sizes of groups to create")
     args = parser.parse_args()
 
     with args.names_file:
-        names = list(map(str.strip, args.names_file.readlines()))
+        members = map(parse_member, args.names_file.readlines())
 
-    shuffled_names = [*names]
-    random.shuffle(shuffled_names)
+    shuffled_members = [*members]
+    random.shuffle(shuffled_members)
 
-    if args.num_groups is not None:
-        groups = divide(shuffled_names, args.num_groups)
-    else:
-        group_sizes = map(int, args.group_sizes.split(','))
-        groups = divide_by_sizes(shuffled_names, group_sizes)
+    group_sizes = map(int, args.group_sizes.split(','))
+    groups = divide_by_sizes(shuffled_members, group_sizes)
 
     for i, group in enumerate(groups):
-        print("Group {}: {}".format(i, ", ".join(group)))
+        print("Group {}: {}".format(i, ", ".join(m.name for m in group)))
